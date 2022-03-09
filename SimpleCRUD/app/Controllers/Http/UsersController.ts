c09201ff5a414859f4593ed80c5c7cb7338a288c
Vicktor Desrony <vldcreation21@gmail.com>
2022-03-09 22:47:20 +0700

    feat: init SimpleCRUD :rocket:

---
+++ SimpleCRUD/app/Controllers/Http/UsersController.ts
@@ -0,0 +1,59 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class UsersController {
+  public async store({ request, response }: HttpContextContract) {
+    const input = request.only(['username', 'email'])
+    try {
+      const users = await User.create(input)
+
+      return response.status(200).json({ code: 200, status: 'success', data: users })
+    } catch (err) {
+      return response.status(500).json({ code: 500, status: 'error', message: err.message })
+    }
+  }
+
+  public async index({response }: HttpContextContract) {
+    try {
+      const users = await User.all()
+
+      return response.status(200).json({ code: 200, status: 'success', data: users })
+    } catch (err) {
+      return response.status(500).json({ code: 500, status: 'error', message: err.message })
+    }
+  }
+
+  public async show({ params, response }: HttpContextContract) {
+    try {
+      const users = await User.find(params.id)
+
+      return response.status(200).json({ code: 200, status: 'success', data: users })
+    } catch (err) {
+      return response.status(500).json({ code: 500, status: 'error', message: err.message })
+    }
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    try {
+      const users = await User.find(params.id)
+      const input = request.only(['username', 'email'])
+      await users?.merge(input)
+      await users?.save()
+
+      return response.status(200).json({ code: 200, status: 'success', data: users })
+    } catch (err) {
+      return response.status(500).json({ code: 500, status: 'error', message: err.message })
+    }
+  }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    try {
+      const users = await User.find(params.id)
+      await users?.delete()
+
+      return response.status(200).json({ code: 200, status: 'success', data: users })
+    } catch (err) {
+      return response.status(500).json({ code: 500, status: 'error', message: err.message })
+    }
+  }
+}
